808af8d51fde70522b7c93ce45a0c031245c24d5
Neus Roman <neusroman@hotmail.com>
2024-08-22 21:37:03 +0200

    add video converter component

---
+++ src/components/VideoConverter.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <input type="file" @change="handleFileChange" accept="video/*" multiple />
+    <video :src="videoUrl" controls></video>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { fetchFile } from '@ffmpeg/util';
+import { FFmpeg } from '@ffmpeg/ffmpeg';
+
+const videoUrl = ref(null);
+const message = ref("");
+const handleFileChange = async (event) => {
+  const files = event.target.files;
+  if (files.length > 0) {
+    const file = files[0];
+    const [convertedFile, fileName] = await convertToMp4(file);
+    message.value = "Done!";
+    videoUrl.value = URL.createObjectURL(convertedFile);
+    console.log(fileName);
+  }
+};
+
+const convertToMp4 = async (file) => {
+  const ext2Convert = ['avi', 'mpg'];
+  const ffmpeg = new FFmpeg({ log: true });
+  message.value = "Loading...";
+  await ffmpeg.load();
+  const fileName = file.name.split('.').slice(0, -1).join('') + '.mp4';
+  if (ext2Convert.includes(file.name.split('.').pop().toLowerCase())) {
+    message.value = "Writting file...";
+    await ffmpeg.writeFile(file.name, await fetchFile(file))
+    message.value = "Executing..."+ fileName;
+    await ffmpeg.exec(['-i', file.name, fileName])
+    message.value = "Reading...";
+    const data = await ffmpeg.readFile(fileName);
+    return [new Blob([data.buffer], { type: 'video/mp4' }), fileName];
+  }
+  return [file, file.name];
+};
+</script>
+
+<style scoped>
+video {
+  width: 600px;
+  aspect-ratio: 16/9;
+  background-color: black;
+}
+</style>
