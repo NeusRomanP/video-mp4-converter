340f07127fc67b4ef845f9efb1e7e3780b6e1842
Neus Roman <neusroman@hotmail.com>
2024-08-23 20:07:47 +0200

    add download video

--- src/components/VideoConverter.vue
+++ src/components/VideoConverter.vue
@@ -3,6 +3,12 @@
     <input type="file" @change="handleFileChange" accept="video/*" multiple />
     <video :src="videoUrl" controls></video>
     <p>{{ message }}</p>
+    <ul>
+      <li v-for="video in videos" :key="video.name">
+        <span @click="playVideo(video)">{{ video.name }}</span>
+        <a :href="video.url" :download="video.name">Download</a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,14 +19,20 @@ import { FFmpeg } from '@ffmpeg/ffmpeg';
 
 const videoUrl = ref(null);
 const message = ref("");
+let videos = ref([]);
 const handleFileChange = async (event) => {
   const files = event.target.files;
   if (files.length > 0) {
-    const file = files[0];
-    const [convertedFile, fileName] = await convertToMp4(file);
+    for (const file of files) {
+      const [convertedFile, fileName] = await convertToMp4(file);
+      videos.value.push({
+        file: convertedFile,
+        name: fileName,
+        url: URL.createObjectURL(convertedFile)
+      });
+    }
     message.value = "Done!";
-    videoUrl.value = URL.createObjectURL(convertedFile);
-    console.log(fileName);
+    console.log("done");
   }
 };
 
@@ -32,14 +44,21 @@ const convertToMp4 = async (file) => {
   const fileName = file.name.split('.').slice(0, -1).join('') + '.mp4';
   if (ext2Convert.includes(file.name.split('.').pop().toLowerCase())) {
     message.value = "Writting file...";
+    console.log("writting file");
     await ffmpeg.writeFile(file.name, await fetchFile(file))
     message.value = "Executing..."+ fileName;
+    console.log("Executing..."+ fileName);
     await ffmpeg.exec(['-i', file.name, fileName])
     message.value = "Reading...";
+    console.log("reading...");
     const data = await ffmpeg.readFile(fileName);
     return [new Blob([data.buffer], { type: 'video/mp4' }), fileName];
   }
   return [file, file.name];
+};
+
+const playVideo = (video) => {
+  videoUrl.value = video.url;
 };
 </script>
 
