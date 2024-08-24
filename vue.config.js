6857a1d7800bdf770f92a993c5c6d8c51b464775
Neus Roman <neusroman@hotmail.com>
2024-08-24 17:48:35 +0200

    change config title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Video MP4 converter', 
+    }
+  }
 })
